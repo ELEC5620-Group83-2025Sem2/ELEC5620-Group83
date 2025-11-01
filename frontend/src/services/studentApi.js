86a884e4ae4e8eb5b7b56c1248db392e954bcbef
LeyuQian <lqia0364@uni.sydney.edu.au>
2025-11-02 05:08:11 +1100

    link backend, display student assignment page (#28)
    * update student/classes display
    
    * add assignment display
    
    can show student's upcoming assignment by its enrolled classes

--- frontend/src/services/studentApi.js
+++ frontend/src/services/studentApi.js
@@ -85,7 +85,20 @@ const studentApi = {
     return authenticatedRequest('/student/classes', {
       method: 'GET'
     });
+  },
+
+  // Assignments
+  // Get all assignments for student's enrolled classes
+  // JWT token is automatically passed in Authorization header
+  async getAssignments(upcoming = false) {
+    return authenticatedRequest('/student/assignments', {
+      method: 'GET',
+      params: {
+        upcoming: upcoming
+      }
+    });
   }
+
 };
 
 export default studentApi;
